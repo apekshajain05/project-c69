9d6f3e7240f8a27c827122b821a2094e6c5911b4
apekshajain05 <62800258+apekshajain05@users.noreply.github.com>
2020-12-28 22:48:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# apeksha-c69
--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,61 +1,77 @@
-
 import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native';
-import * as Permission from 'expo-permissions';
-import {BarCodeScanner} from 'expo-barcode-scanner';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default class App extends React.Component {
+export default class TransactionScreen extends React.Component {
     constructor(){
-        super();
-        this.state={
-            hasCameraPermission:null,
-            scanned:false,
-            scannedData:'',
-            buttonState:'normal',
-        }
+      super();
+      this.state = {
+        hasCameraPermissions: null,
+        scanned: false,
+        scannedData: '',
+        buttonState: 'normal'
+      }
     }
-    getCameraPermission=async()=>{
-        const {status}=await Permission.askAsync(Permission.CAMERA);
-        this.setState({
-            hasCameraPermission:status==='granted',
-        })
+
+    getCameraPermissions = async () =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA);
+      
+      this.setState({
+        /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+        hasCameraPermissions: status === "granted",
+        buttonState: 'clicked',
+        scanned: false
+      });
     }
-    handleBarcodeScanner=(type,data)=>{
-        this.setState({
-            scanned:'true',
-            buttonState:'clicked',
-            scannedData:data,
-        })
+
+    handleBarCodeScanned = async({type, data})=>{
+      this.setState({
+        scanned: true,
+        scannedData: data,
+        buttonState: 'normal'
+      });
     }
-  render(){
-    const hasCameraPermission=this.state.hasCameraPermission;
-    const scanned=this.state.scanned;
-    const buttonState=this.state.buttonState;
-    if(buttonState==='granted'&&hasCameraPermission){
+
+    render() {
+      const hasCameraPermissions = this.state.hasCameraPermissions;
+      const scanned = this.state.scanned;
+      const buttonState = this.state.buttonState;
+
+      if (buttonState === "clicked" && hasCameraPermissions){
         return(
-            <BarCodeScanner onBarCodeScanned={scanned?undefined:this.handleBarcodeScanner} />
-        )
-    }
-    else if(buttonState==='normal'){
-  return (
-    <View style={styles.container}>
-          <Text>{
-              hasCameraPermission==='granted'?this.state.scannedData:"Request Camera Access"
-           } </Text>
-          <TouchableOpacity onPress={this.getCameraPermission} >
-          <Text>Scan QR code  </Text>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        );
+      }
+
+      else if (buttonState === "normal"){
+        return(
+          <View style={styles.container}>
+
+          <Text style={styles.displayText}>{
+            hasCameraPermissions===true ? this.state.scannedData: "Request Camera Permission"
+          }</Text>     
+
+          <TouchableOpacity
+            onPress={this.getCameraPermissions}>
+            <Text style={styles.buttonText}>Scan QR Code</Text>
           </TouchableOpacity>
         </View>
-  );
-}
+        );
+      }
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    
+  });
